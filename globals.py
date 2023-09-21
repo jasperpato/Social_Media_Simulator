6ef6b90dfd8ce58cfb44bf22504c665a5413a259
Jasper Paterson <jasperpato@gmail.com>
2023-09-21 10:23:07 +0800

    ok

--- globals.py
+++ globals.py
@@ -1,2 +1,14 @@
+NUM_AGENTS = 1000
 NUM_OPINIONS = 10
-POST_GEN_NOISE = 0.2
+
+# making posts
+PROPORTION_POSTING_AGENTS = 0.1
+POST_GENERATION_NOISE = 0.2
+
+# choosing posts for an agent
+# posts that align with the agent's opinions have higher probability of being chosen
+# posts that align with the platform's opinions have higher probability of being chosen for any agent
+
+# consuming posts
+POSTS_PER_DAY = 50
+POST_INFLUENCE = 0.1
--- utils.py
+++ utils.py
@@ -4,3 +4,7 @@ from globals import *
 def get_random_opinions() -> np.ndarray:
 	'''return np.ndarray of floats in [-1, 1)'''
 	return 2 * np.random.rand(NUM_OPINIONS) - 1
+
+def similarity(a: np.ndarray, b: np.ndarray) -> float:
+	'''returns the cosine similarity between [0, 1] between two opinion vectors'''
+	return np.dot(a, b) / np.linalg.norm(a) / np.linalg.norm(b)
