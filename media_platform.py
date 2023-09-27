103d24ac945f1f81b17fc8371b967eb9987961f0
Allen <allenantony2001@gmail.com>
2023-09-28 03:00:33 +0800

    define agent class

--- media_platform.py
+++ media_platform.py
@@ -1,14 +1,22 @@
 import numpy as np
+import random
 import networkx as nx
+
+from agent import Agent
 
 NUM_AGENTS = 100
 K = 10
+PROBABILITY_BIASED = 0.1
+
 
 class MediaPlatform():
 	def __init__(self):
 		self.graph = nx.random_regular_graph(K, NUM_AGENTS)
 		for n in self.graph:
-			self.graph.nodes[n]['opinion'] = np.random.choice([-1, 1])
+			if random.random() > PROBABILITY_BIASED:
+				self.graph.nodes[n]['self'] = Agent()
+			else: 
+				self.graph.nodes[n]['self'] = Agent(is_biased=True)
 
 if __name__ == '__main__':
 	m = MediaPlatform()
