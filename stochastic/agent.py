e291809ed0eec20ec8dbbbc48dd6393c628e174f
Jasper Paterson <jasperpato@gmail.com>
2023-10-03 17:28:50 +0800

    ok!

--- stochastic/agent.py
+++ stochastic/agent.py
@@ -8,12 +8,15 @@ class Agent:
         self.signal_mixes = [1 if P < np.random.random() else 0]
         self.update_orientation()
 
+    def incongruent(self, signal_mix):
+        return (signal_mix < 0.5 and self.orientation == 1) or (signal_mix >= 0.5 and self.orientation == -1)
+
     def receive_mix(self, signal_mix):
         '''
         Receives a signal mix from another agent and updates its own signal mix accordingly
         '''
-        if self.is_biased:
-            signal_mix = 2 * Q * abs(signal_mix - 0.5)
+        if self.is_biased and self.incongruent(signal_mix):
+            signal_mix += 2 * Q * abs(signal_mix - 0.5) * (self.orientation)
         self.signal_mixes.append(signal_mix)
 
     def update_orientation(self):
