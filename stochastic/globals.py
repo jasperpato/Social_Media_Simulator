a8e738253e93fe8abee079c7d8e5aeea2596ad80
Jasper Paterson <jasperpato@gmail.com>
2023-10-04 09:54:24 +0800

    ok

--- stochastic/globals.py
+++ stochastic/globals.py
@@ -1,9 +1,8 @@
-NUM_AGENTS = 500
+NUM_AGENTS = 100
 NUM_TIME_STEPS = 100
 
 K = 0.3 # proportion of posting agents
 P = 0.6 # proportion of agents initially supporting ground truth
-B = 0.1 # proportion of biased agents
+B = 0.3 # proportion of biased agents
 Q = 0.5 # rejection probability of a biased agent
-
 I = 0 # probability of platform censoring an incongruent post
