c7b6af02928c48391eb2494ddc413d5ea132d07f
Jasper Paterson <jasperpato@gmail.com>
2023-10-06 17:07:53 +0800

    ok

--- continuous/media_platform.py
+++ continuous/media_platform.py
@@ -9,35 +9,46 @@ class MediaPlatform():
 		self.verbose = verbose
 		self.agents = [Agent(is_poster = i < NUM_POSTERS) for i in range(NUM_AGENTS)]
 		
-		# for convergence
 		self.prev_opinions = [a.opinion for a in self.agents]
 		self.num_same = 0
 
 	def time_step(self):
+		'''
+		Each agent consumes the same subset of posts
+		'''
 		self.posts = [a.opinion for a in self.agents if a.is_poster][:POSTS_PER_DAY]
 		for a in self.agents:
 			for p in self.posts:
 				a.consume_post(p)
 
+	def converged(self):
+		'''
+		Return True if no opinions have changed in the last CONVERGENCE_NUM time steps
+		'''
+		opinions = [a.opinion for a in self.agents]
+		if opinions == self.prev_opinions:
+			self.num_same += 1
+		else:
+			self.prev_opinions = opinions
+			self.num_same = 0
+
+		return self.num_same == CONVERGENCE_NUM
+
 	def simulate(self):
+		'''
+		Execute the specified time steps
+		'''
 		for i in range(NUM_TIME_STEPS):
 			if self.verbose:
 				print(f'Time step {i}')
-
 			self.time_step()
-
-			# test for convergence
-			opinions = [a.opinion for a in self.agents]
-			if opinions == self.prev_opinions:
-				self.num_same += 1
-			else:
-				self.prev_opinions = opinions
-				self.num_same = 0
-
-			if self.num_same == CONVERGENCE_NUM:
+			if self.converged():
 				return
 
 	def fractions(self):
+		'''
+		Return the fractions of agents holding the extreme opinions [-1, 1]
+		'''
 		pos = len([a for a in self.agents if a.opinion == 1])
 		neg = len([a for a in self.agents if a.opinion == -1])
 
@@ -48,11 +59,17 @@ class MediaPlatform():
 		return [pos, neg]
 
 	def graph(self):
+		'''
+		Graph every agent's opinion changing over time
+		'''
 		_, ax = plt.subplots()
 		for i, a in enumerate(self.agents):
 			ax.plot(a.opinions, label=str(i))
 
 def simulate():
+	'''
+	Execute an entire simulation
+	'''
 	m = MediaPlatform()
 	m.simulate()
 	m.graph()
