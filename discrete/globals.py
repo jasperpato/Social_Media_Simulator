1a6bbb497178d70658b677f5ed647e14020a846c
Jasper Paterson <jasperpato@gmail.com>
2023-10-11 10:03:18 +0800

    structure

--- discrete/globals.py
+++
@@ -1,8 +0,0 @@
-NUM_AGENTS = 100
-NUM_TIME_STEPS = 100
-
-K = 0.3 # proportion of posting agents
-P = 0.55 # proportion of agents initially supporting ground truth
-B = 0.3 # proportion of biased agents
-Q = 0.5 # rejection probability of a biased agent
-I = 0 # probability of platform censoring an incongruent post
