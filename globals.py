046966ef344d01ad6ce37ccdafa594e09cb1d38a
Allen <allenantony2001@gmail.com>
2023-10-14 19:12:04 +0800

    cleaning up globals file

--- globals.py
+++ globals.py
@@ -1,29 +1,20 @@
+
+# ----------------- simulation parameters ----------------------#
+
 NUM_AGENTS = 500
 NUM_TIME_STEPS = 1000
 NUM_SIMULATIONS = 500
+POLARISATION_CUTOFF = 0.75	# agents with opinions outside [-cutoff, cutoff] are considered polarised
+CONVERGENCE_NUM = 10		# number of time steps with no change to end the simulation
 
-# number of time steps with no change to end the simulation
-CONVERGENCE_NUM = 10
 
-# proportion of posting agents
-P = 0.5
+# ----------------- default hyperparameters ---------------------------#
 
-# standard deviation of Gaussian noise applied to posts
-POST_NOISE = 0
+B = 0		# agent bias
+P = 0.3		# proportion of posting agents
+N = 0		# standard deviation of Gaussian noise applied to posts
+C = 0.1		# proportion of posts consumed per day
+D = 0.1		# change in agent's opinion due to a post
+PB = 1		# platform bias
+RB = 1		# recommendation bias
 
-# proportion of posts consumed per day
-C = 0.1
-
-# change in agent's opinion due to a post
-D = 0.1
-
-# agent bias
-B = 0
-
-# biased platform parameters
-PLATFORM_BIAS = 0
-PLATFORMS = [round(i / 10, 1) for i in range(0, 51)] # 0-5 in steps of 0.1
-
-RECOMMENDATION_BIAS = 1
-
-POLARISATION_CUTOFF = 0.75
